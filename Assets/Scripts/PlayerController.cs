8d4519b53c571c0b08c213a8b41bbe15fbab1f6b
Rae <ra508729@ucf.edu>
2026-02-21 21:18:54 -0500

    Update PlayerController.cs
    can now press "Interact" button

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,37 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(PlayerInput))]
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private PlayerInput input;
+
+    private void Awake()
     {
-        
+        input = GetComponent<PlayerInput>();
     }
 
-    // Update is called once per frame
-    void Update()
+    //use commented version to use project-wide input mapping (doesn't currently work)
+    private void OnEnable()
     {
-        
+        input.actions["Interact"].performed += OnInteract;
+        input.actions["Interact"].Enable();
+        // InputSystem.actions["Interact"].performed += OnInteract;
+        // InputSystem.actions["Interact"].Enable();
+
+    }
+
+    //use commented version to use project-wide input mapping (doesn't currently work)
+    private void OnDisable()
+    {
+        input.actions["Interact"].performed -= OnInteract;
+        input.actions["Interact"].Disable();
+        // InputSystem.actions["Interact"].performed -= OnInteract;
+        // InputSystem.actions["Interact"].Disable();
+    }
+
+    private void OnInteract(InputAction.CallbackContext ctx)
+    {
+        Debug.Log("interact pressed");
     }
 }
