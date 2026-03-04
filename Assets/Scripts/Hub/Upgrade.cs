ea49d6d42a458e56b529cf625ac11af0cf05657d
Rae <ra508729@ucf.edu>
2026-03-04 13:17:16 -0500

    create Upgrade SO
    all upgrades will follow this template (to be expanded)

---
+++ Assets/Scripts/Hub/Upgrade.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Upgrade", menuName = "Scriptable Objects/Upgrade")]
+public class Upgrade : ScriptableObject
+{
+    public string upgradeName;
+    public string description;
+    public int cost;
+}
