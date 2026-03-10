9e0a1cf03e8671b5b3cc2c2a3854219f3e55b33b
Rae <ra508729@ucf.edu>
2026-03-09 21:12:29 -0400

    convert shop to prefab
    condensed several gameObjects into one. Allowed trigger to look for a component rather than requiring serialized field

--- Assets/Scripts/Hub/ShopTrigger.cs
+++ Assets/Scripts/Hub/ShopTrigger.cs
@@ -1,15 +1,22 @@
 using UnityEngine;
 
+[RequireComponent(typeof(ShopUI))]
 public class ShopTrigger : MonoBehaviour
 {
-    [SerializeField]
     private ShopUI shopUI;
+
+    public void Awake()
+    {
+        shopUI = GetComponent<ShopUI>();
+    }
 
     //TODO change to allow player to open shop while in trigger zone, rather than force-opening it
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Player"))
         {
+            
+            
             shopUI.Show();
         }
     }
