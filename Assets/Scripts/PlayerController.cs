25cc0c8f78250d2c2ec55e15aef135fa7d7d2645
Rae <ra508729@ucf.edu>
2026-03-11 22:04:42 -0400

    reimplement project-wide input map
    I didn't change anything... I was curious so I tried and it just started working?? whatever

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,39 +4,34 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(PlayerInput))]
 public class PlayerController : MonoBehaviour
 {
-    private PlayerInput input;
+    //the default input mapping suddenly started working again... keep the temporary stuff available in case it breaks again
+    //private PlayerInput input;
     public static bool canMove = true; //set to false to prevent movement (like when in a shop)
 
     private void Awake()
     {
-        input = GetComponent<PlayerInput>();
+        //input = GetComponent<PlayerInput>();
     }
 
-    //use commented version to use project-wide input mapping (doesn't currently work)
+    //use input.actions instead of InputSystem.actions if default action mapping stops working again
     private void OnEnable()
     {
-        input.actions["Save"].performed += OnSave;
-        input.actions["Save"].Enable();
+        InputSystem.actions["Save"].performed += OnSave;
+        InputSystem.actions["Save"].Enable();
 
-        input.actions["Load"].performed += OnLoad;
-        input.actions["Load"].Enable();
-
-        // InputSystem.actions["Interact"].performed += OnInteract;
-        // InputSystem.actions["Interact"].Enable();
+        InputSystem.actions["Load"].performed += OnLoad;
+        InputSystem.actions["Load"].Enable();
 
     }
 
-    //use commented version to use project-wide input mapping (doesn't currently work)
+    //use input.actions instead of InputSystem.actions if default action mapping stops working again
     private void OnDisable()
     {
-        input.actions["Save"].performed -= OnSave;
-        input.actions["Save"].Disable();
+        InputSystem.actions["Save"].performed -= OnSave;
+        InputSystem.actions["Save"].Disable();
 
-        input.actions["Load"].performed -= OnLoad;
-        input.actions["Load"].Disable();
-
-        // InputSystem.actions["Interact"].performed -= OnInteract;
-        // InputSystem.actions["Interact"].Disable();
+        InputSystem.actions["Load"].performed -= OnLoad;
+        InputSystem.actions["Load"].Disable();
     }
 
     private void OnSave(InputAction.CallbackContext ctx)
