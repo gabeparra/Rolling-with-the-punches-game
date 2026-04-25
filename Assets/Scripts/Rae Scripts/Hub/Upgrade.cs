54c63973ab178a15dd48c7adae8b76917e86f365
Gabriel Parra <ma.parrar16@gmail.com>
2026-04-25 19:52:20 -0400

    I think all the merging went well? Only tested hector scene, will come back today, hector rae and nahum all merged, gotta rebase from this most likely

--- Assets/Scripts/Rae Scripts/Hub/Upgrade.cs
+++ Assets/Scripts/Rae Scripts/Hub/Upgrade.cs
@@ -5,15 +5,6 @@ public class Upgrade : ScriptableObject
 {
     public string upgradeName;
     public string description;
-    public int cost;
-    public UpgradeType type;
-    [Tooltip("Effect value (e.g. 0.25 = 25% reload-speed reduction)")]
-    public float value;
-
-    public enum UpgradeType
-    {
-        ReloadSpeed,
-        Damage,
-        MaxAmmo
-    }
+    public int[] prices;
+    public int maxLevel;
 }
