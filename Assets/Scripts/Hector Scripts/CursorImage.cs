0c45911f8babbd1476ada5088a919d2e682d3a07
Rae <60368869+x-rae@users.noreply.github.com>
2026-05-01 02:43:37 -0400

    fix cursor icon centering
    sets cursor hotspot to center of image, regardless of size

--- Assets/Scripts/Hector Scripts/CursorImage.cs
+++ Assets/Scripts/Hector Scripts/CursorImage.cs
@@ -7,9 +7,11 @@ public class CursorImage : MonoBehaviour
 {
     public Texture2D cursorTexture; // This is to store the image data for the sprite
     public Vector2 hotspot = Vector2.zero; // Helps to calibrate the cursor image on screen
+    public bool autoCenterHotspot = false;
 
     void Start()
     {
+        if(autoCenterHotspot) hotspot = new Vector2(cursorTexture.width/2, cursorTexture.height/2);
         Cursor.SetCursor(cursorTexture, hotspot, CursorMode.Auto); // Binds the image, hotspot, and sets cursor to automode.
         Cursor.visible = true; // Toggles if cursor is visual or not -- good for testing.
     }
